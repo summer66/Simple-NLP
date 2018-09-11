b59630f7ee28ec1302f9cf32d72161e9ed42f674
summer66 <winterbellyh@gmail.com>
2018-09-11 18:39:38 +0000

    file and bean

---
+++ src/MeshVocab.java
@@ -0,0 +1,26 @@
+import com.univocity.parsers.annotations.Parsed;
+
+public class MeshVocab {
+
+    @Parsed(index = 0)
+    private String preferred;
+
+    @Parsed(index = 1)
+    private String synonyms;
+
+    @Parsed(index = 2)
+    private String parents;
+
+    @Parsed(index = 0)
+    private String semanticTypes;
+
+    @Override
+    public String toString() {
+        return "MeshVocab{" +
+                "preferred='" + preferred + '\'' +
+                ", synonyms='" + synonyms + '\'' +
+                ", parents='" + parents + '\'' +
+                ", semanticTypes='" + semanticTypes + '\'' +
+                '}';
+    }
+}
